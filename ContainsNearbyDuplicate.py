4818374dfb16146ff593758c02da10188f4d2fb9
DoritozX <lzbhjh123@gmail.com>
2017-02-22 22:02:14 +0900

    leetcode/array

---
+++ ContainsNearbyDuplicate.py
@@ -0,0 +1,16 @@
+class ContainsNearbyDuplicate(object):
+    """
+    Given an array of integers and an integer k, find out whether there are two distinct indices i and j in the array such that nums[i] = nums[j] and the absolute difference between i and j is at most k.
+    """
+    def containsNearbyDuplicateMineSolution(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+        dic = {}
+        for i, v in enumerate(nums):
+            if v in dic and i - dic[v] <= k:
+                return True
+            dic[v] = i
+        return False
